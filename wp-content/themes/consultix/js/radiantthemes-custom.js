1d78bcf7008f08781e0892492de78a1b46b8dc47
kuba <info@weblead.pl>
2022-01-22 17:50:19 +0100

    pierwszy

--- wp-content/themes/consultix/js/radiantthemes-custom.js
+++ wp-content/themes/consultix/js/radiantthemes-custom.js
@@ -178,7 +178,7 @@ if(document.URL=="http://itdb.biz/referencje/")
 
 	$( ".z1" ).click(function() {
 		$( ".z1 img" ).animate({
-		  scale: 200,
+		  transform: scale(2) ,
 		  left: "",
 		  height: ""
 		}, 1000, function() {
